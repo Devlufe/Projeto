3b2f0591243e7e139ac03a9513ef7b6e2edf7b3c
gilsonduartesenac <146297517+gilsonduartesenac@users.noreply.github.com>
2024-04-30 21:11:19 -0300

    settingsComplementos

--- projeto_fonte/settings.py
+++ projeto_fonte/settings.py
@@ -113,7 +113,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'fotos')
 
 
 # Default primary key field type
