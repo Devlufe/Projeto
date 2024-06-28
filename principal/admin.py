79a2418fd057904c64e008e5c2586b64b2fd4269
iRonaldMoura <ronaldolliveira@icloud.com>
2024-06-28 20:26:16 -0300

    Revert "Merge branch 'main' of https://github.com/Devlufe/Projeto"
    This reverts commit 7711cfeb6690b93f465dfc4b9883fd5cfb20aea6, reversing
    changes made to fffdbeb4433131757f0caa9b25205e576bbe6111.

--- principal/admin.py
+++ principal/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import CategoriaFotos, Fotografo, Foto, AgendaServico
 
-admin.site.register(CategoriaFotos)
-admin.site.register(Fotografo)
-admin.site.register(Foto)
-admin.site.register(AgendaServico)
+# Register your models here.
--- principal/urls.py
+++ principal/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from .import views
-from .views import enviar_foto # Importe ambas as funções do mesmo módulo
-from django.conf import settings
-from django.conf.urls.static import static
-
-
+from . import views
 
 
 
@@ -16,8 +11,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('sample-inner-page/', views.sampleinnerpage, name='sample-inner-page'),
     path('services/', views.services, name='services'),
-
-    path('enviar-foto/', enviar_foto, name='enviar_foto'),
     
-    # Outras URLs do seu aplicativo...
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
